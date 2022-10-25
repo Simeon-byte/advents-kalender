05d06464d8ae0fc2ea7baeb775eb62b7dbf5f74d
Semmel1005 <samuel.peschl@student.htldornbirn.at>
2022-10-25 12:10:31 +0200

    a

--- scripts/clicky.js
+++ scripts/clicky.js
@@ -1,3 +1,100 @@
 function door1() {
     alert("MEMEMEMEMEME");
 };
+
+function door2() {
+    alert("MEMEMEMEMEME");
+};
+
+function door3() {
+    alert("MEMEMEMEMEME");
+};
+
+function door4() {
+    alert("MEMEMEMEMEME");
+};
+
+function door5() {
+    alert("MEMEMEMEMEME");
+};
+
+function door6() {
+    alert("MEMEMEMEMEME");
+};
+
+function door7() {
+    alert("MEMEMEMEMEME");
+};
+
+function door8() {
+    alert("MEMEMEMEMEME");
+};
+
+function door9() {
+    alert("MEMEMEMEMEME");
+};
+
+function door10() {
+    alert("MEMEMEMEMEME");
+};
+
+function door11() {
+    alert("MEMEMEMEMEME");
+};
+
+function door12() {
+    alert("MEMEMEMEMEME");
+};
+
+function door13() {
+    alert("MEMEMEMEMEME");
+};
+
+function door14() {
+    alert("MEMEMEMEMEME");
+};
+
+function door15() {
+    alert("MEMEMEMEMEME");
+};
+
+function door16() {
+    alert("MEMEMEMEMEME");
+};
+
+function door17() {
+    alert("MEMEMEMEMEME");
+};
+
+function door18() {
+    alert("MEMEMEMEMEME");
+};
+
+function door19() {
+    alert("MEMEMEMEMEME");
+};
+
+function door20() {
+    alert("MEMEMEMEMEME");
+};
+
+function door21() {
+    alert("MEMEMEMEMEME");
+};
+
+function door22() {
+    alert("MEMEMEMEMEME");
+};
+
+function door23() {
+    alert("MEMEMEMEMEME");
+};
+
+function door24() {
+    alert("MEMEMEMEMEME");
+};
+
+
+
+
+
