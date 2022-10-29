a8af890f23ea0e83b1adc98c12701550b7b307d5
Semmel1005 <samuel.peschl@student.htldornbirn.at>
2022-10-29 20:46:56 +0200

    kleine unwichtige änderung

--- scripts/data.js
+++ scripts/data.js
@@ -1,13 +1,12 @@
 const data = {
 
 
-  //15
   "0": { "color": "rgb(47,118,147)", "classNames": "lichterkette" },
   "1": { "color": "rgb(112,128,131)", "classNames": "" },
   "2": { "color": "rgb(60,109,201)", "classNames": "" },
   "3": { "color": "rgb(227,233,238)", "classNames": "adventskranz" },
   "4": { "color": "rgb(9, 94, 173)", "classNames": "" },
-  "5": { "color": "rgb(3, 82, 255)", "classNames": "" },
+  "5": { "color": "rgb(3, 82, 255)", "classNames": "nikolaus" },
   "6": { "color": "rgb(95, 90, 199)", "classNames": "" },
   "7": { "color": "rgb(98, 124, 188)", "classNames": "" },
   "8": { "color": "rgb(47,118,147)", "classNames": "" },
@@ -25,13 +24,9 @@ const data = {
   "20": { "color": "rgb(9, 94, 173)", "classNames": "" },
   "21": { "color": "rgb(3, 82, 255)", "classNames": "" },
   "22": { "color": "rgb(95, 90, 199)", "classNames": "" },
-  "23": { "color": "rgb(98, 124, 188)", "classNames": "" },
+  "23": { "color": "rgb(98, 124, 188)", "classNames": "weihnachten" },
 
 
-
-
-
-
-  
+//weihnachten und nikolaus sind nur temporär und kann ma spöter no ändern!!!!!!!
 
 };
