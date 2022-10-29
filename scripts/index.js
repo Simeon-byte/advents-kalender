ce4f50162da2320d59990f97a0ff9e28e81b880b
Simeon-byte <saimjen.duglas@gmail.com>
2022-10-29 19:43:10 +0200

    pushhhh

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,61 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  data.length = Object.keys(data).length;
+document.addEventListener('DOMContentLoaded', async () => {
+    data.length = Object.keys(data).length;
 
-  // generate grid-items:
-  // <button class="grid-item" type="button" onclick="handleClick(event)">
-  //        <p class="doorLink"></p>
-  // </button>
-  const gridParent = document.getElementsByClassName("grid-container")[0];
-  for (let i = 0; i < data.length; i++) {
-    let button = document.createElement("button");
-    button.classList.add("grid-item");
-    button.onclick = (event) => handleClick(event);
+    // generate grid-items:
+    // <button class="grid-item" type="button" onclick="handleClick(event)">
+    //        <p class="doorLink"></p>
+    // </button>
+    const gridParent = document.getElementsByClassName('grid-container')[0];
+    for (let i = 0; i < data.length; i++) {
+        let button = document.createElement('button');
+        button.classList.add('grid-item');
+        button.onclick = (event) => handleClick(event);
 
-    let p = document.createElement("p");
-    p.classList.add("doorLink");
-    button.appendChild(p);
-    gridParent.appendChild(button);
-  }
-
-  const doors = document.getElementsByClassName("grid-item");
-
-  let prev = [];
-  for (let i = 0; i < data.length; i++) {
-    let doorIndex = Math.floor(Math.random() * doors.length);
-
-    while (prev.includes(doorIndex)) {
-      doorIndex = Math.floor(Math.random() * doors.length);
+        let p = document.createElement('p');
+        p.classList.add('doorLink');
+        button.appendChild(p);
+        gridParent.appendChild(button);
     }
-    let door = doors[doorIndex];
-    prev.push(doorIndex);
 
-    door.children[0].textContent = i + 1;
-    door.style.backgroundColor = data[i].color;
-    door.dataset.num = i + 1;
+    const doors = document.getElementsByClassName('grid-item');
 
-    let dateString = `2022-12-${i + 1}`;
-    let enabledDate = new Date(dateString);
+    let prev = [];
+    for (let i = 0; i < data.length; i++) {
+        let doorIndex = Math.floor(Math.random() * doors.length);
 
-    let nowDate =  new Date("2022-12-12"); // Date.now()
-    if (nowDate >= enabledDate) {
-      // date check for enabled state
-      // date passed -> button enabled
-    } else {
-      door.disabled = true;
-    }   
-  }
+        while (prev.includes(doorIndex)) {
+            doorIndex = Math.floor(Math.random() * doors.length);
+        }
+        let door = doors[doorIndex];
+        prev.push(doorIndex);
+
+
+        // add classNames specified in data.js object to door element
+        let ClassNames = data[i].classNames.trim().split(' ');
+        ClassNames[0] != "" && door.classList.add(...ClassNames);
+
+        door.children[0].textContent = i + 1;
+        door.style.backgroundColor = data[i].color;
+        door.dataset.num = i + 1;
+
+        let dateString = `2022-12-${i + 1}`;
+        let enabledDate = new Date(dateString);
+
+        let nowDate = new Date('2022-12-12'); // Date.now()
+        if (nowDate >= enabledDate) {
+            // date check for enabled state
+            // date passed -> button enabled
+        } else {
+            door.disabled = true;
+        }
+    }
 });
 
 const handleClick = (event) => {
-  console.log(data[event.target.dataset.num]); // reference to data object
-  if (true) {
-    // date check etc.
-    window.location.href = "./doors/door" + event.target.dataset.num + ".html";
-  }
+    console.log(data[event.target.dataset.num]); // reference to data object
+    if (true) {
+        // date check etc.
+        window.location.href =
+            './doors/door' + event.target.dataset.num + '.html';
+    }
 };
-
